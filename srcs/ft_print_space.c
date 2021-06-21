5c4e2ff8315ddf6bf37cabbbc282224117cc1308
Charles Mariot <contact@charles-mariot.fr>
2021-06-21 16:53:22 +0200

    AutoSave

--- srcs/ft_print_space.c
+++ srcs/ft_print_space.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 17:05:47 by cmariot           #+#    #+#             */
-/*   Updated: 2021/06/20 13:09:26 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/06/21 16:52:58 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_print_space(t_flags *flags, int len)
 	if (len < flags->field_width)
 	{
 		c = ' ';
-		while (flags->field_width-- - len)
+		while (flags->field_width-- - len && flags->precision--)
 		{
 			flags->total_lenght += write(1, &c, 1);
 		}
--- srcs/ft_print_str.c
+++ srcs/ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 18:32:19 by cmariot           #+#    #+#             */
-/*   Updated: 2021/06/19 20:33:45 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/06/21 16:53:01 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,12 @@ void	ft_print_str(t_flags *flags)
 		ft_star_flag(flags);
 	str = va_arg(flags->args, char *);
 	len = ft_strlen(str);
+	printf("precision = %d\n", flags->precision);	
 	if (flags->field_width && !flags->minus_flag && !flags->dot_flag)
 		ft_print_space(flags, ft_strlen(str));
-	if (flags->dot_flag)
-		while (*str && flags->field_width--)
-			flags->total_lenght += ft_putchar(*str++);
-	else
-		while (*str)
-			flags->total_lenght += ft_putchar(*str++);
+
+	while (*str)
+		flags->total_lenght += ft_putchar(*str++);
 	if (flags->field_width && flags->minus_flag && !flags->dot_flag)
 	{
 		ft_print_space(flags, len);
