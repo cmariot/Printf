dc08694d27dedb9269d4d08e32b6a656115c1c5e
Charles Mariot <contact@charles-mariot.fr>
2021-07-05 18:47:30 +0200

    AutoSave

--- srcs/ft_initialize_flags.c
+++ srcs/ft_initialize_flags.c
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 16:50:13 by cmariot           #+#    #+#             */
-/*   Updated: 2021/07/01 13:59:12 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/07/05 18:25:47 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ t_flags	*ft_initialize_flags(t_flags *flags)
 	flags->star_for_precision = 0;
 	flags->precision = 0;
 	flags->total_print = 0;
+	flags->minus_printed = 0;
 	return (flags);
 }
--- srcs/ft_print_integer.c
+++ srcs/ft_print_integer.c
@@ -6,16 +6,63 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/17 19:23:50 by cmariot           #+#    #+#             */
-/*   Updated: 2021/07/05 13:31:26 by cmariot          ###   ########.fr       */
+/*   Updated: 2021/07/05 18:47:21 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_print_space_for_integer(t_flags *flags, int len)
+void	ft_space_before_integer(t_flags *flags, int len, char *str)
 {
 	char	c;
 	int		i;
+
+	// The number of spaces to print before the integer is influenced by :
+	// - The field_width given
+	// 		. If fw > len or precision
+	// 		. If the zero flag is present we print 0 and not spaces
+	// 		. 
+
+	if (flags->zero && !flags->minus)
+	{
+		c = '0';
+		if (*str == '-')
+		{
+			flags->total_print += ft_putchar('-');
+			flags->minus_printed = 1;
+		}
+	}
+	else
+		c = ' ';
+	i = 0;
+	if (flags->field_width > flags->precision)
+	{
+		if (len < flags->field_width)
+		{
+			if (flags->field_width > flags->precision)
+				while (flags->field_width-- - len)
+					flags->total_print += write(1, &c, 1);
+		}
+		else if (len > flags->precision)
+		{
+			if (flags->field_width > len)
+				while ((flags->field_width - len) - i++)
+					flags->total_print += write(1, &c, 1);
+		}
+		else if (flags->precision > len)
+		{
+			if (flags->field_width > flags->precision)
+				while ((flags->field_width - flags->precision) - i++)
+					flags->total_print += write(1, &c, 1);
+		}
+		flags->field_width = 0;
+	}
+}
+
+void	ft_space_after_integer(t_flags *flags, int len)
+{
+	int		i;
+	char	c;
 
 	c = ' ';
 	i = 0;
@@ -37,7 +84,7 @@ void	ft_print_space_for_integer(t_flags *flags, int len)
 			while (flags->field_width-- - len)
 				flags->total_print += write(1, &c, 1);
 	}
-	flags->field_width = 0;
+	return ;
 }
 
 void	ft_treat_integer(char *str, t_flags *flags)
@@ -47,7 +94,7 @@ void	ft_treat_integer(char *str, t_flags *flags)
 
 	len = ft_strlen(str);
 	if (flags->field_width && !flags->minus)
-		ft_print_space_for_integer(flags, len);
+		ft_space_before_integer(flags, len, str);
 	if (flags->dot)
 	{
 		i = 0;
@@ -55,26 +102,22 @@ void	ft_treat_integer(char *str, t_flags *flags)
 		{
 			if (*str == '-' && flags->precision)
 			{
-				flags->total_print += ft_putchar(*str++);
-				// Pas sur du i ici
-				i--;
+				flags->total_print += ft_putchar('-');
+				flags->minus_printed = 1;
 			}
 			while ((flags->precision - len) - i++)
-				// Ici a corriger, afficher des espaces si fw ?
 				flags->total_print += ft_putchar('0');
 		}
 		else if (!flags->precision)
 			while (*str == '0')
 				str++;
-		while (*str)
-			flags->total_print += ft_putchar(*str++);
-
 	}
-	else
-		while (*str)
-			flags->total_print += ft_putchar(*str++);
+	if (flags->minus_printed)
+		str++;
+	while (*str)
+		flags->total_print += ft_putchar(*str++);
 	if (flags->field_width && flags->minus)
-		ft_print_space_for_integer(flags, len);
+		ft_space_after_integer(flags, len);
 	flags->minus = 0;
 	flags->zero = 0;
 }
